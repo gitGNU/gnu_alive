eb9a286432694e1a051dee30c6636d80cc10a3ac
Joachim Nilsson <joachim.nilsson@member.fsf.org>
2004-01-03 22:32:28 +0000

    Clean up.

--- include/config.h
+++ include/config.h
@@ -8,6 +8,38 @@
 
 struct config_data
 {
+  char *username;
+  char *password;
+  char *login_server;
+  int   server_port;
+
+  /* These four build the login_string in http.c:internet_login() */
+  char *login_string_header;
+  char *username_key;
+  char *password_key;
+  char *login_string_footer;
+
+  /* Search reply for successful login/logout */
+  char *logged_in_string;
+  char *logged_out_string;
+
+  char *init_page;
+  char *login_page;
+  char *logout_page;
+
+  /* Are we logged in? */
+  int  logged_in;
+
+  /* Disgusting static communication buffers */
+  char send_msg[MAXDATASIZE], get_msg[MAXDATASIZE];
+
+  /* Start the keep-alive daemon? */
+  int daemon_start;
+  /* "Ping" or login every @daemon_delay minutes? */
+  int daemon_type;
+  /* Sleep for X minutes before pinging or logging in again. */
+  int daemon_delay;
+
   /* The config file, usually /etc/qadsl.conf */
   char *conf_file;
 
@@ -16,22 +48,6 @@ struct config_data
 
   /* Socket we're connected to the login_server with. */
   int  sockfd;
-  char *login_server;
-  int   server_port;
-
-  char *init_page;
-  char *login_page;
-  char *logout_page;
-
-  int  logged_in;
-  char send_msg[MAXDATASIZE], get_msg[MAXDATASIZE];
-
-  char *username;
-  char *password;
-
-  int daemon_start;
-  int daemon_type;
-  int daemon_delay;
 };
 
 typedef struct config_data config_data_t;
