d0b74a333f94f896462bd1be3a8cd2fccc8e7f78
Joachim Nilsson <joachim.nilsson@member.fsf.org>
2004-03-03 18:59:57 +0000

    Need to increase size of static log buffer. Also, add copyrights and licensing.

--- src/log.c
+++ src/log.c
@@ -1,3 +1,22 @@
+/* log.c - Handles all communication with the outside world.
+ *
+ * Copyright (C) 2003, 2004 Joachim Nilsson <joachim!nilsson()member!fsf!org>
+ *
+ * qADSL is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * qADSL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
@@ -14,7 +33,7 @@
 void
 write_log (char *fmt, ...)
 {
-  char str[80]; /* XXX - What is this, a hardcoded value?! */
+  char str[MAXDATASIZE]; /* XXX - What is this, a hardcoded value?! */
   va_list ap;
 
   va_start (ap, fmt);
