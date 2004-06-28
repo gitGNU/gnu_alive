7ced97f961971521ee432a26d86fc97681944baa
Joachim Nilsson <joachim.nilsson@member.fsf.org>
2004-06-28 22:51:09 +0000

    misc.

--- lib/strcasecmp.c
+++ lib/strcasecmp.c
@@ -1,6 +1,6 @@
-/* 
- * scratch implementation of strcasecmp(), 
- * in case your C library doesn't have it 
+/*
+ * scratch implementation of strcasecmp(),
+ * in case your C library doesn't have it
  *
  * For license terms, see the file COPYING in this directory.
  */
@@ -15,17 +15,24 @@
 strcasecmp(char *s1, char *s2)
 {
     while (toupper(*s1) == toupper(*s2++))
-	if (*s1++ == '\0')
-	    return(0);
+        if (*s1++ == '\0')
+            return(0);
     return(toupper(*s1) - toupper(*--s2));
 }
 
 strncasecmp(char *s1, char *s2, register int n)
 {
     while (--n >= 0 && toupper(*s1) == toupper(*s2++))
-	if (toupper(*s1++) == '\0')
-	    return(0);
+        if (toupper(*s1++) == '\0')
+            return(0);
     return(n < 0 ? 0 : toupper(*s1) - toupper(*--s2));
 }
 
 #endif /* !HAVE_STRCASECMP */
+
+/* Local Variables:
+ * mode: C;
+ * c-file-style: gnu;
+ * indent-tabs-mode: nil;
+ * End:
+ */
