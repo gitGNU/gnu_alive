61a3c246adc46c41e29f2df5eedce1c016b1c208
Joachim Nilsson <joachim.nilsson@member.fsf.org>
2004-06-29 10:17:53 +0000

    Cleanup and added new option, -p,--port

--- src/main.c
+++ src/main.c
@@ -1,27 +1,22 @@
-/* qADSL - An automatic login daemon for WAN/LAN/ADSL connections
+/*
+ * qADSL - An automatic login daemon for WAN/LAN/ADSL connections
  *
- * qADSL is an auto-login/connection-keep-alive daemon for various
- * WAN/LAN/ADSL connections provided by several (Swedish) ISPs.
- * To mention a few: Telia ADSL, COMHEM, Tiscali. All of which are
- * based on the commercial product "Orbyte Wireless System" from the
- * Swedish company Åkerströms nowire AB - see <http://www.nowire.se>
+ * Copyright (c) 2001 Jakob "kuba" Stasilowicz <kuba()unix!se> Copyright (c)
+ * 2003,2004 Joachim Nilsson <joachim!nilsson()member!fsf!org>
  *
- * Copyright (c) 2001 Jakob "kuba" Stasilowicz <kuba()unix!se>
- * Copyright (c) 2003,2004 Joachim Nilsson <joachim!nilsson()member!fsf!org>
+ * qADSL is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  *
- * qADSL is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.
+ * qADSL is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
  *
- * qADSL is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
 #ifdef HAVE_CONFIG_H
@@ -38,10 +33,31 @@
 #include "getopt.h"
 #include "process.h"
 
-
+#define USAGE printf ("Usage: %s [-h] [-c conffile] [-p pidfile] [-loPsdvV]\n", program_name);
 static void usage (int status);
 
-char *program_name;
+
+static const char *program_name;
+
+
+#ifndef HAVE_RINDEX
+const char *get_program_name (const char *progname)
+{
+  return progname;
+}
+#else
+const char *get_program_name (const char *progname)
+{
+  char *clean_name;
+
+  clean_name = rindex (progname, '/');
+  if (clean_name)
+    return clean_name + 1;
+
+  return progname;
+}
+#endif
+
 
 
 int
@@ -49,46 +65,44 @@ main (int argc, char *argv[])
 {
   extern char *optarg;
   config_data_t *config;
-  char *pid_file  = NULL;
+  char *pid_file = NULL;
+  int port = -1;
   char *conf_file = NULL;
   int verbose = MSG_NONE;
   op_t operation = NOP;
-  char c;
+  int c;
 
-  struct option const long_options[] =
+  struct option const long_options[] = {
+    {"login", no_argument, 0, 'l'},
+    {"logout", no_argument, 0, 'o'},
+    {"conf-file", required_argument, 0, 'c'},
+    {"pid-file", required_argument, 0, 'p'},
+    {"port", required_argument, 0, 'P'},
+    {"status", no_argument, 0, 's'},
+    {"version", no_argument, 0, 'V'},
+    {"verbose", no_argument, 0, 'v'},
+    {"debug", no_argument, 0, 'd'},
+    {"help", no_argument, 0, 'h'},
+    {"usage", no_argument, 0, 'u'},     /* Same as --help, for now */
+    {NULL, 0, NULL, 0}
+  };
+
+  program_name = get_program_name (argv[0]);
+
+  do
     {
-      {"login", no_argument, 0, 'l'},
-      {"logout", no_argument, 0, 'o'},
-      {"conf-file", required_argument, 0, 'c'},
-      {"pid-file", required_argument, 0, 'p'},
-      {"status", no_argument, 0, 's'},
-      {"version", no_argument, 0, 'V'},
-      {"verbose", no_argument, 0, 'v'},
-      {"debug", no_argument, 0, 'd'},
-      {"help", no_argument, 0, 'h'},
-      {NULL, 0, NULL, 0}
-    };
+      c = getopt_long (argc, argv, "l"  /* login */
+                       "o"      /* logout */
+                       "c:"     /* conf-file */
+                       "p:"     /* pid-file */
+                       "P:"     /* port */
+                       "s"      /* status */
+                       "v"      /* verbose */
+                       "V"      /* version */
+                       "d"      /* debug */
+                       "h?",    /* help */
+                       long_options, NULL);
 
-#ifdef HAVE_RINDEX
-  program_name = rindex (argv[0], '/');
-  if (program_name) 
-    program_name ++;
-  else
-#endif
-    program_name = argv[0];
-
-  while ((c = getopt_long (argc, argv,
-                           "l"  /* login */
-                           "o"  /* logout */
-                           "c:" /* conf-file */
-                           "p:" /* pid-file */
-                           "s"  /* status */
-                           "v"  /* verbose */
-                           "V"  /* version */
-                           "d"  /* debug */
-                           "h?", /* help */
-                           long_options, (int *)0)) != EOF)
-    {
       switch (c)
         {
         case 'l':               /* Login */
@@ -124,16 +138,32 @@ main (int argc, char *argv[])
           pid_file = optarg;
           break;
 
+        case 'P':
+          port = atoi (optarg);
+          break;
+
         case '?':
         case 'h':
-        default:
           usage (EXIT_SUCCESS);
           break;
+
+        case 'u':
+          USAGE;
+          exit (EXIT_SUCCESS);
+          break;
+
+        case EOF:
+          break;
+
+        default:
+          ERROR ("Unrecognised option, %s", argv[0]);
+          usage (EXIT_FAILURE);
         }
     }
+  while (EOF != c);
 
   /* Read /etc/qadsl.conf or similar */
-  if (operation == NOP) 
+  if (operation == NOP)
     {
       usage (EXIT_FAILURE);
     }
@@ -143,39 +173,46 @@ main (int argc, char *argv[])
     {
       exit (EXIT_FAILURE);
     }
-
-  /* Override any qadsl.conf settings or configure --with-pidfile
-   * if the user specified a different PID file on the command line.
-   */
-  if (pid_file)
-    {
-      config->pid_file = pid_file;
-    }
+  /* Override qadsl.conf and default values when given on command line. */
+  if (pid_file)   config->pid_file = pid_file;
+  if (port != -1) config->server_port = port;
 
   /* Handle request */
   return process (config, operation, verbose);
 }
 
+
 static void
 usage (int status)
 {
-  printf ("%s - Auto-login & keep-alive for Internet connections.\n\n", 
-          program_name);
-  printf ("Usage: %s [-h] [-c conffile] [-p pidfile] [-losvV]\n", 
-          program_name);
+  printf ("%s - Auto-login & keep-alive for Internet connections.\n\n", program_name);
+
+  USAGE;
+
   printf ("\
 Options:\n\
 -l, --login               Try to login\n\
 -o, --logout              Try to logout\n\
 -c, --conf-file=FILE      Use settings from FILE instead of " GLOBAL_CONF "\n\
--p, --pid-file=FILE       Use FILE to store PID\n\
+-p, --pid-file=FILE       Use FILE to store PID, default is " PID_FILE "\n\
+-P, --port=PORT           Connect to PORT on server, default is " PORT "\n\
 -s, --status              Display status of qadsl daemon\n\
 -v, --verbose             Display more information, on screen and in logfile\n\
+-d, --debug               Display even more information, huge amounts!\n\
 -V, --version             Display version information and exit\n\
 -h, --help                Display this help and exist\n\
+    --usage               Give a shore usage message\n\
 \n\
 Report bugs to <" PACKAGE_BUGREPORT ">\n\
 ");
 
   exit (status);
 }
+
+
+/* Local Variables:
+ * mode: C;
+ * c-file-style: gnu;
+ * indent-tabs-mode: nil;
+ * End:
+ */
