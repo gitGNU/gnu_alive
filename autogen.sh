d4cb7b1a14847d779a1346c2522f7795d5ef5681
Joachim Nilsson <joachim.nilsson@member.fsf.org>
2004-10-16 15:58:28 +0000

    Minor change - copy files, don't synlink.

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,7 @@
 # Does all the funky stuff of creating every generated file of
 # the GNU Configure & Build System.
 #
-# Add libtoolize and friends when we move to that.
-#
 
 rm -rf autom4te.cache
 gettextize --intl -f -c
-autoreconf -f -i -s -W gnu
-
+autoreconf -f -i -W gnu
