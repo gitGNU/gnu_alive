c27781ba6ff70e6a60decf897980c10c6b21d6f6
Joachim Nilsson <joachim.nilsson@member.fsf.org>
2005-02-21 21:41:19 +0000

    Add funky sed script to modify intl/Makefile.in if autogen:ed.

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,6 @@
 #
 
 rm -rf autom4te.cache
-gettextize --intl -f -c
+gettextize --intl -f -c && sed "s/\(^INCLUDES = -I\..*$\)/\1 -I../include/
+s/\(^$(OBJECTS): ..\/\)\(config\.h.*$\)/\1include\/\2/" <intl/Makefile.in >intl/Makefile.new && mv intl/Makefile.new intl/Makefile.in && echo "intl/Makefile.in modified successfully for GNU Alive."
 autoreconf -f -i -W portability
-
